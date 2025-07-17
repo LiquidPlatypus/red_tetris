29d25ef544868a68d79bfc52add22187a7438f30
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-07-17 14:00:39 +0200

    buttons + permanent title

--- frontend/red-tetris/src/views/Lobby.vue
+++ frontend/red-tetris/src/views/Lobby.vue
@@ -1,17 +1,30 @@
 <script setup>
 
 import {ref} from 'vue';
+import {useRouter} from "vue-router";
+import ApppButton from "@/components/ApppButton.vue";
+
+const router = useRouter();
+
+function createGame() {
+	router.push('/game');
+}
 
 </script>
 
 <template>
 	<main class="lobby">
-		<div>
-			<h1 class="title">SIMPLYTIS</h1>
+		<div class="gameChoice">
+			<ApppButton @click="createGame">LAUNCH GAME</ApppButton>
+			<ApppButton>COPY LINK</ApppButton>
 		</div>
 	</main>
 </template>
 
 <style scoped>
+	.gameChoice {
+		display: grid;
+		gap: 1vh;
+	}
 
 </style>
