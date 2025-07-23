74967db729cdb2de390d6e99adcb57c4e4e4a619
lecreepyyy <vincentx758@gmail.com>
2025-07-23 13:57:38 +0200

    rm old file

--- static/client.js
+++
@@ -1,9 +0,0 @@
-const socket = io(); // va se connecter au même host:port
-
-socket.on('connect', () => {
-    console.log('🤠​ Connected !');
-});
-
-socket.on('disconnect', () => {
-    console.log('😵​ Disconnected.');
-});
