be882f44c59bafdd09491d837630f84d3393bbb0
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-07-28 10:17:14 +0200

    unused import

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, computed, onMounted } from "vue";
 import AppButton from "@/components/AppButton.vue";
-import App from "@/App.vue";
 
 const ROWS = 20;
 const COLS = 10;
