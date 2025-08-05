337f08b6e8e66c283d6b88ab9b949bfca73a1a62
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-08-05 12:45:01 +0200

    endgame waiting

--- frontend/src/views/EndGame.vue
+++ frontend/src/views/EndGame.vue
@@ -1,8 +1,11 @@
 <script setup>
+import {ref} from "vue";
 import { useRouter } from "vue-router";
 import AppButton from "@/components/AppButton.vue";
 
 const router = useRouter();
+
+const allPlayerFinished = ref(false);
 
 function retry() {
 	router.push("/game");
@@ -12,7 +15,13 @@ function retry() {
 <template>
 	<main class="endgame">
 		<div class="game-over">
-			<h2>GAME OVER !</h2>
+			<div>
+				<!-- TABLEAU -->
+			</div>
+			<h2 v-if="!allPlayerFinished">WAITING FOR OTHER PLAYERS TO FINISH
+				<span class="dot-typing"></span>
+			</h2>
+			<h2 v-if="allPlayerFinished">GAME OVER !</h2>
 
 			<!-- A ENLEVER -->
 			<AppButton @click="retry">RETRY</AppButton>
@@ -28,7 +37,35 @@ function retry() {
 	grid-column: 1 / 4;
 	grid-row: 5;
 	text-align: center;
-	color: #ff0000;
+	color: red;
 	font-weight: bold;
+}
+
+.dot-typing {
+	display: inline-block;
+}
+
+.dot-typing::after {
+	content: '...';
+	animation: dots-blink 3s steps(4, end) infinite;
+	letter-spacing: 2px;
+}
+
+@keyframes dots-blink {
+	0% {
+		content: '';
+	}
+	25% {
+		content: '.';
+	}
+	50% {
+		content: '..';
+	}
+	75% {
+		content: '...';
+	}
+	100% {
+		content: '';
+	}
 }
 </style>
