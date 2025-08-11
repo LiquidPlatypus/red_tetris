a0475d387ead22d32137c44336216ecd8b78d4de
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-08-11 13:32:43 +0200

    style BeforeRoom.vue

--- frontend/src/views/BeforeRoom.vue
+++ frontend/src/views/BeforeRoom.vue
@@ -2,7 +2,6 @@
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import AppButton from "@/components/AppButton.vue";
-import socket from "@/socket.js";
 
 const router = useRouter();
 const route = useRoute();
@@ -10,27 +9,57 @@ const pseudo = ref('');
 const seed = route.params.seed;
 
 function joinLobby() {
-    if (pseudo.value.trim() === '') return;
-    console.log(`/${seed}/${pseudo.value}`);
-    router.push(`/${seed}/${pseudo.value}`);
+	if (pseudo.value.trim() === '')
+		return;
+	console.log(`/${seed}/${pseudo.value}`);
+	router.push(`/${seed}/${pseudo.value}`);
 }
 
 </script>
 
 <template>
-
-<div id="pseudo-request">
-    <input
-	id="sendUsername"
-	v-model="pseudo"
-	class="pseudo-input"
-	type="text"
-	placeholder="Pseudo"
-	/>
-    <AppButton @click="joinLobby">JOIN GAME</AppButton>
-</div>
+	<main>
+		<div id="pseudo-request">
+			<input
+				id="sendUsername"
+				v-model="pseudo"
+				class="pseudo-input"
+				type="text"
+				placeholder="Pseudo"
+			/>
+			<AppButton @click="joinLobby">JOIN GAME</AppButton>
+		</div>
+	</main>
 
 </template>
 
-<style>
+<style scoped>
+main {
+	font-family: Pixel, sans-serif;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 20px;
+}
+
+#pseudo-request {
+	display: grid;
+	gap: 1vh;
+}
+
+input::placeholder {
+	font-family: "ModernTetris", sans-serif;
+	font-size: 0.8rem;
+	color: darkgray;
+}
+
+.pseudo-input {
+	font-family: Pixel, sans-serif;
+	font-size: 15px;
+	border-left: #5454541a solid 3px;
+	border-top: #5454541a solid 3px;
+	border-right: #5454547a solid 3px;
+	border-bottom: #5454547a solid 3px;
+	height: 4vh;
+}
 </style>
