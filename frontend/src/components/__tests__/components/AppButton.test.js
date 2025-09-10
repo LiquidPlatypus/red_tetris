d943d652cac52a22132dae191dd8685e0d83a22c
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-09-10 13:24:44 +0200

    moved files into a directory

--- frontend/src/components/__tests__/components/AppButton.test.js
+++ frontend/src/components/__tests__/components/AppButton.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
-import AppButton from "../AppButton.vue";
+import AppButton from "../../AppButton.vue";
 
 describe("AppButton", () => {
 	let wrapper;
