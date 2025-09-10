fede5191d5e5ddef3e8abb2523d30d7991279200
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-09-10 13:27:40 +0200

    let instead of const cause of error

--- frontend/src/views/Lobby.vue
+++ frontend/src/views/Lobby.vue
@@ -8,7 +8,7 @@ import { askServer } from "@/utils";
 const router = useRouter()
 const route = useRoute()
 const seed = route.params.seed;
-const username = route.params.username;
+let username = route.params.username;
 if (username.length > 12) {
 	// const confirmPseudo = window.confirm("Your username is too long (max 12 characters)\nIt will be trunced\nContinue ?");
 	const confirmPseudo = window.prompt("Entre ton pseudo :", "Joueur1");
