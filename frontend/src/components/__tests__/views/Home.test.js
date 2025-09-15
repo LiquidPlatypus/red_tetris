4664b76ee0920db4971941e54d4afbc36cb72724
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-09-15 14:11:20 +0200

    WIP Home view unit test

--- frontend/src/components/__tests__/views/Home.test.js
+++ frontend/src/components/__tests__/views/Home.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import { createRouter, createWebHistory } from "vue-router"
 import { routes } from "@/router"
@@ -9,3 +9,24 @@ const router = createRouter({
 	routes
 })
 
+vi.mock("@/socket.js", () => {
+	const mockSocket = {
+		on: vi.fn(),
+		emit: vi.fn(),
+		_events: new Map(),
+
+		_trigger: function(event, ...args) {
+			const handlers = this._events.get(event) || [];
+			handlers.forEach(handler => handler(...args));
+		}
+	};
+
+	mockSocket.on.mockImplementation((event, handler) => {
+		if (!mockSocket._events.has(event)) {
+			mockSocket._events.set(event, []);
+		}
+		mockSocket._events.get(event).push(handler);
+	});
+
+	return { default: mockSocket };
+});
