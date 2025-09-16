15c0f8293a1e23282256477880c13a9d5d6d5a11
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-09-16 13:56:33 +0200

    WIP functions unit test index

--- frontend/src/components/__tests__/router/index.test.js
+++ frontend/src/components/__tests__/router/index.test.js
@@ -1,5 +1,8 @@
 import { describe, it, expect } from "vitest";
+import {mount} from "@vue/test-utils";
 import router from "@/router";
+import BeforeRoom from "@/views/BeforeRoom.vue";
+import Lobby from "@/views/Lobby.vue";
 
 describe("router", () => {
 	it("should have Home route", () => {
@@ -29,5 +32,23 @@ describe("router navigation", () => {
 	it("resolve Home route", async () => {
 		const route = router.resolve({ name: "Home" });
 		expect(route.href).toBe("/");
+	});
+});
+
+describe("route content", () => {
+	it.skip("should show BeforeRoom", async () => {
+		const wrapper = mount(BeforeRoom);
+
+		expect(wrapper.find(".pseudo-input").exists()).toBe(true);
+	})
+
+	it.skip("should show Lobby", async () => {
+		const wrapper = mount(Lobby);
+
+		expect(wrapper.find("gameChoice").exists()).toBe(true);
+	});
+
+	it.skip("should show 404", async () => {
+		const wrapper = mount();
 	});
 });
