a417a0b6b67ffbb9ef6c5b50f7af003fe1acb325
Liquid Platypus <thibaut.bournonville@gmail.com>
2025-09-16 10:56:15 +0200

    finished Home view unit test

--- frontend/src/components/__tests__/views/Home.test.js
+++ frontend/src/components/__tests__/views/Home.test.js
@@ -4,11 +4,6 @@ import { createRouter, createWebHistory } from "vue-router"
 import { routes } from "@/router"
 import Home from "../../../views/Home.vue";
 
-const router = createRouter({
-	history: createWebHistory(),
-	routes
-})
-
 vi.mock("@/socket.js", () => {
 	const mockSocket = {
 		on: vi.fn(),
@@ -29,4 +24,85 @@ vi.mock("@/socket.js", () => {
 	});
 
 	return { default: mockSocket };
+});
+
+import socket from "@/socket.js";
+
+describe("Home.vue", () => {
+	let wrapper;
+	let router;
+
+	beforeEach(async () => {
+		vi.clearAllMocks();
+		socket._events.clear();
+
+		socket.emit = vi.fn();
+		socket.on = vi.fn().mockImplementation((event, handler) => {
+			if (!socket._events.has(event)) {
+				socket._events.set(event, []);
+			}
+			socket._events.get(event).push(handler);
+		});
+
+		router = createRouter({
+			history: createWebHistory(),
+			routes: [
+				{ path: "/", component: { template: "<div>Home</div>" } },
+				// { path: `/${info.Seed}/${info.Username}`, component: { template: "<div></div>" } }
+			]
+		});
+
+		await router.push("/");
+
+		wrapper = mount(Home, {
+			global: {
+				plugins: [router]
+			}
+		});
+
+		await router.isReady();
+	});
+
+	afterEach(async () => {
+		vi.clearAllMocks();
+		if (socket._events)
+			socket._events.clear();
+
+		if (wrapper)
+			wrapper.unmount();
+	});
+
+	describe("Initialisation du composant", () => {
+		it("should save all socket listeners on mount", () => {
+			expect(socket.on).toHaveBeenCalledWith("lobby-join", expect.any(Function));
+		});
+	});
+
+	describe("createLobby()", () => {
+		it ("should not emit if empty alias", async () => {
+			const input = wrapper.find("input#msgInput");
+			await input.setValue("");
+			await wrapper.find("button").trigger("click");
+
+			expect(socket.emit).not.toHaveBeenCalledWith();
+		});
+
+		it("should emit if non-empty alias", async () => {
+			const input = wrapper.find("input#msgInput");
+			await input.setValue("VVPOIROT");
+			await wrapper.find("button").trigger("click");
+
+			expect(socket.emit).toHaveBeenCalledWith("create-lobby", "VVPOIROT");
+		});
+	});
+
+	describe("socket event lobby-join", () => {
+		it("should redirect to the correct route", async () => {
+			const pushSpy = vi.spyOn(router, "push");
+
+			socket._trigger("lobby-join", { Seed: "123", Username: "VVPOIROT" });
+
+			expect(pushSpy).toHaveBeenCalled("/123/VVPOIROT");
+		});
+	});
 });
